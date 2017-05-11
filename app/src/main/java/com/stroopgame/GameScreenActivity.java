2d42dcaabd0637e692de044c485cec92f75c366c
mahmadzadeh <mahmadzadeh@gmail.com>
2017-05-10 20:54:06 -0700

    Trying out GridView for the main game activity

--- app/src/main/java/com/stroopgame/GameScreenActivity.java
+++ app/src/main/java/com/stroopgame/GameScreenActivity.java
@@ -1,5 +1,27 @@
 package com.stroopgame;
 
-public class GameScreenActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
+public class GameScreenActivity  extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(GameScreenActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
